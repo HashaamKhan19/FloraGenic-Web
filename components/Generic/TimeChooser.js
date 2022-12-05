48b97bbc381343190c76a702b2edb042834f4ab2
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2022-12-05 23:51:43 +0500

    Made TimeChooser controlled

--- components/Generic/TimeChooser.js
+++ components/Generic/TimeChooser.js
@@ -1,16 +1,16 @@
-import * as React from 'react'
-import dayjs from 'dayjs'
-import TextField from '@mui/material/TextField'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { TimePicker } from '@mui/x-date-pickers/TimePicker'
+import * as React from "react";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 export default function TimeChooser() {
-  const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'))
+  const [value, setValue] = React.useState(dayjs("2014-08-18T21:11:54"));
 
   const handleChange = (newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -21,5 +21,5 @@ export default function TimeChooser() {
         renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
-  )
+  );
 }
