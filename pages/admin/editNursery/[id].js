65451ff3dfd3060abef5ceee54c472467f391475
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-25 22:25:50 +0500

    Made nursery form autofill on edit

--- pages/admin/editNursery/[id].js
+++ pages/admin/editNursery/[id].js
@@ -1,10 +1,41 @@
-import React from 'react'
-import AddNursery from '../../../components/Admin/AddNursery'
+import React from "react";
+import AddNursery from "../../../components/Admin/AddNursery";
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+import Loader from "../../../components/Generic/Loader";
+const GET_NURSERY_BY_ID = gql`
+  query Nursery($nurseryId: ID!) {
+    nursery(id: $nurseryId) {
+      id
+      nurseryOwnerID
+      name
+      details
+      openingHours
+      closingHours
+      rating
+      address
+      phoneNumber
+      email
+      website
+      images
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 export default function EditNursery() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { loading, error, data } = useQuery(GET_NURSERY_BY_ID, {
+    variables: { nurseryId: id },
+  });
+
+  if (loading) return <Loader />;
+  console.log(data);
   return (
     <>
-      <AddNursery />
+      <AddNursery data={data?.nursery} />
     </>
-  )
+  );
 }
