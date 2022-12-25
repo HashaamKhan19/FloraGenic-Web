c44a607796bbe0183544a660f5705cbf47b30548
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-25 22:31:02 +0500

    Removed required from non required fields and added url regex

--- components/Admin/AddNursery.js
+++ components/Admin/AddNursery.js
@@ -350,7 +350,6 @@ const AddNursery = ({ data = {} }) => {
                 <InputLabel
                   htmlFor="phoneNumber"
                   variant="standard"
-                  required
                   sx={{
                     mb: 1.5,
                     color: "text.primary",
@@ -361,7 +360,6 @@ const AddNursery = ({ data = {} }) => {
                 </InputLabel>
                 <ControlledTelInput
                   control={control}
-                  required
                   defaultCountry="PK"
                   id="phoneNumber"
                   name="phoneNumber"
@@ -376,7 +374,6 @@ const AddNursery = ({ data = {} }) => {
                 <InputLabel
                   htmlFor="email"
                   variant="standard"
-                  required
                   sx={{
                     mb: 1.5,
                     color: "text.primary",
@@ -387,7 +384,6 @@ const AddNursery = ({ data = {} }) => {
                 </InputLabel>
                 <ControlledTextInput
                   control={control}
-                  required
                   pattern={/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/}
                   id="email"
                   name="email"
@@ -458,7 +454,6 @@ const AddNursery = ({ data = {} }) => {
                 <InputLabel
                   htmlFor="website"
                   variant="standard"
-                  required
                   sx={{
                     mb: 1.5,
                     color: "text.primary",
@@ -469,9 +464,11 @@ const AddNursery = ({ data = {} }) => {
                 </InputLabel>
                 <ControlledTextInput
                   control={control}
-                  required
                   id="website"
                   name="website"
+                  pattern={
+                    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+                  }
                   fullWidth
                   autoComplete="Website URL"
                   error={errors.website ? true : false}
