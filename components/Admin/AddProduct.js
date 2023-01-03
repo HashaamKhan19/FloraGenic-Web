f4486f56f9faad3c2ad379d3935985ebc9104a8f
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2023-01-03 21:15:35 +0500

    Refactored button background

--- components/Admin/AddProduct.js
+++ components/Admin/AddProduct.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Grid from "@mui/material/Unstable_Grid2";
-import { TextField, InputLabel, Select, MenuItem } from "@mui/material";
+import {
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { AddProductIcon } from "../../public/icons/AddProductIcon";
 import { styled } from "@mui/material/styles";
 import Chip from "@mui/material/Chip";
@@ -14,7 +20,9 @@ import ControlledDropzone from "../Generic/ControlledComponents/ControlledDropzo
 import ControlledSelect from "../Generic/ControlledComponents/ControlledSelect";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import Loader from "../Generic/Loader";
-
+import MultiDropzone from "../Generic/MultiDropzone";
+import ControlledMultiDropzone from "../Generic/ControlledComponents/ControlledMultiDropzone";
+import ButtonBackground from "../../assets/Pattern/ButtonBackground";
 const ListItem = styled("li")(({ theme }) => ({
   margin: theme.spacing(0.5),
 }));
@@ -434,37 +442,22 @@ const AddProduct = ({ data = {} }) => {
                   })}
                 </div>
               </Grid>
+              <Grid item xs={12}>
+                <ControlledMultiDropzone
+                  control={control}
+                  getValues={getValues}
+                  setValue={setValue}
+                  required
+                  name="image"
+                  id="image"
+                  error={errors.image ? true : false}
+                  helperText="At least 1 image is required"
+                />
+              </Grid>
 
               <Grid item xs={12} textAlign="center" sx={{ mt: 2, p: 2 }}>
                 <button class="relative px-6 py-2 font-medium text-white transition duration-300 bg-green-500 rounded-md hover:bg-floraGreen ease">
-                  <span class="absolute bottom-0 left-0 h-full -ml-2">
-                    <svg
-                      viewBox="0 0 487 487"
-                      class="w-auto h-full opacity-100 object-stretch"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M0 .3c67 2.1 134.1 4.3 186.3 37 52.2 32.7 89.6 95.8 112.8 150.6 23.2 54.8 32.3 101.4 61.2 149.9 28.9 48.4 77.7 98.8 126.4 149.2H0V.3z"
-                        fill="#FFF"
-                        fill-rule="nonzero"
-                        fillOpacity=".1"
-                      ></path>
-                    </svg>
-                  </span>
-                  <span class="absolute top-0 right-0 w-12 h-full -mr-3">
-                    <svg
-                      viewBox="0 0 487 487"
-                      class="object-cover w-full h-full"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M487 486.7c-66.1-3.6-132.3-7.3-186.3-37s-95.9-85.3-126.2-137.2c-30.4-51.8-49.3-99.9-76.5-151.4C70.9 109.6 35.6 54.8.3 0H487v486.7z"
-                        fill="#FFF"
-                        fill-rule="nonzero"
-                        fillOpacity=".1"
-                      ></path>
-                    </svg>
-                  </span>
+                  <ButtonBackground />
                   <AddProductIcon sx={{ mt: 0.6 }} fontSize="medium" />
                   <span class="relative">{action} Product</span>
                 </button>
