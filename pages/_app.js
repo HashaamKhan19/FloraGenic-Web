a6540fd2fee83a20d651d594d515ec6df192bc3c
Hashaam Khan <hashaamkhan4247@gmail.com>
2023-02-04 02:21:47 +0500

    removed customer components, recreating it from start

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,19 @@
-import "../styles/globals.css";
-import Sidebar from "../components/Admin/Sidebar";
-import GardenerSidebar from "../components/Gardener/Sidebar";
-import NurserySidebar from "../components/Nursery/Sidebar";
-import Header from "../components/Customer/Header";
-import Footer from "../components/Customer/Footer";
-import { ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { theme } from "../theme/theme";
+import '../styles/globals.css'
+import Sidebar from '../components/Admin/Sidebar'
+import GardenerSidebar from '../components/Gardener/Sidebar'
+import NurserySidebar from '../components/Nursery/Sidebar'
+import { ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { theme } from '../theme/theme'
 
 function MyApp({ Component, pageProps, router }) {
   const client = new ApolloClient({
-    uri: "https://floragenic.herokuapp.com/graphql",
+    uri: 'https://floragenic.herokuapp.com/graphql',
     cache: new InMemoryCache(),
-  });
+  })
 
-  if (router.pathname.startsWith("/admin")) {
+  if (router.pathname.startsWith('/admin')) {
     return (
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
@@ -24,10 +22,10 @@ function MyApp({ Component, pageProps, router }) {
           </Sidebar>
         </ThemeProvider>
       </ApolloProvider>
-    );
+    )
   }
 
-  if (router.pathname.startsWith("/gardener")) {
+  if (router.pathname.startsWith('/gardener')) {
     return (
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
@@ -36,10 +34,10 @@ function MyApp({ Component, pageProps, router }) {
           </GardenerSidebar>
         </ThemeProvider>
       </ApolloProvider>
-    );
+    )
   }
 
-  if (router.pathname.startsWith("/nursery")) {
+  if (router.pathname.startsWith('/nursery')) {
     return (
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
@@ -48,19 +46,17 @@ function MyApp({ Component, pageProps, router }) {
           </NurserySidebar>
         </ThemeProvider>
       </ApolloProvider>
-    );
+    )
   }
 
-  if (router.pathname.startsWith("/customer")) {
+  if (router.pathname.startsWith('/customer')) {
     return (
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
-          <Header />
           <Component {...pageProps} />
-          <Footer />
         </ThemeProvider>
       </ApolloProvider>
-    );
+    )
   }
 
   return (
@@ -69,7 +65,7 @@ function MyApp({ Component, pageProps, router }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/customer/index.js
+++ pages/customer/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import Hero from '../../components/Customer/Hero'
-import FlashDeals from '../../components/Customer/FlashDeals'
-import TopRated from '../../components/Customer/TopRated'
-import TopCategories from '../../components/Customer/TopCategories'
-import { Box } from '@mui/system'
 
 export default function index() {
   return (
-    <>
-      <Box sx={{ backgroundColor: '#F6F9FC', height: '100vh' }}>
-        <Hero />
-        <FlashDeals />
-        <TopCategories />
-        <TopRated />
-      </Box>
-    </>
+    <div>
+      <p>This is customer page</p>
+    </div>
   )
 }
