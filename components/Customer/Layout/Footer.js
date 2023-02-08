efff56972a0c178ad79d7cb5a0e0cd5a3460c7f1
Hashaam Khan <hashaamkhan4247@gmail.com>
2023-02-08 15:12:50 +0500

    added responsiveness to footer

--- components/Customer/Layout/Footer.js
+++ components/Customer/Layout/Footer.js
@@ -1,263 +1,170 @@
+import { createStyles, Text, Container, ActionIcon, Group } from '@mantine/core'
 import {
-  ActionIcon,
-  Anchor,
-  Box,
-  Button,
-  createStyles,
-  Grid,
-  Group,
-  Stack,
-  Text,
-} from '@mantine/core'
-import Image from 'next/image'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
-import { BsFacebook } from 'react-icons/bs'
-import { FaGooglePlay } from 'react-icons/fa'
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from '@tabler/icons'
 import FloraGenicLogo from '../../../public/Logo/floraGenic.png'
+import Image from 'next/image'
+import data from '../Generic/FooterLinks'
+import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs'
 
 const useStyles = createStyles((theme) => ({
   footer: {
-    marginTop: 50,
+    marginTop: 120,
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
     borderTop: `1px solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
     }`,
-    backgroundColor: 'rgba(98, 168, 44, 0.3)',
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    padding: '30px',
+  },
+
+  logo: {
+    maxWidth: 200,
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  description: {
+    marginTop: 5,
+
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.xs,
+      textAlign: 'center',
+    },
   },
 
   inner: {
     display: 'flex',
     justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  groups: {
+    display: 'flex',
+    flexWrap: 'wrap',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  wrapper: {
+    width: 200,
+  },
+
+  link: {
+    display: 'block',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+    fontSize: theme.fontSizes.sm,
+    paddingTop: 3,
+    paddingBottom: 3,
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+
+  title: {
+    fontSize: theme.fontSizes.lg,
+    fontWeight: 700,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    marginBottom: theme.spacing.xs / 2,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  afterFooter: {
+    display: 'flex',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '0 50px',
+    marginTop: theme.spacing.xl,
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
 
     [theme.fn.smallerThan('sm')]: {
       flexDirection: 'column',
     },
   },
 
-  links: {
-    fontWeight: 600,
-    fontSize: 20,
+  social: {
     [theme.fn.smallerThan('sm')]: {
-      marginTop: theme.spacing.lg,
-      marginBottom: theme.spacing.sm,
+      marginTop: theme.spacing.xs,
     },
   },
 }))
 
 export default function Footer() {
-  const aboutUsLinks = [
-    {
-      link: '#',
-      label: 'Careers',
-    },
-    {
-      link: '#',
-      label: 'Our Stores',
-    },
-    {
-      link: '#',
-      label: 'Our Cares',
-    },
-    {
-      link: '#',
-      label: 'Terms & Conditions',
-    },
-    {
-      link: '#',
-      label: 'Privacy Policy',
-    },
-  ]
-
-  const customerCareLinks = [
-    {
-      link: '#',
-      label: 'Help Center',
-    },
-    {
-      link: '#',
-      label: 'How to Buy',
-    },
-    {
-      link: '#',
-      label: 'Track your Order',
-    },
-    {
-      link: '#',
-      label: 'Corporate & Bulk Purchasing',
-    },
-    {
-      link: '#',
-      label: 'Returns & Refunds',
-    },
-  ]
-
   const { classes } = useStyles()
-  const aboutUsItems = aboutUsLinks.map((link) => (
-    <Anchor
-      color={'dimmed'}
-      key={link.label}
-      href={link.link}
-      sx={{ lineHeight: 1 }}
-      onClick={(event) => event.preventDefault()}
-      size="sm"
-    >
-      {link.label}
-    </Anchor>
-  ))
 
-  const customerCareItems = customerCareLinks.map((link) => (
-    <Anchor
-      color={'dimmed'}
-      key={link.label}
-      href={link.link}
-      sx={{ lineHeight: 1 }}
-      onClick={(event) => event.preventDefault()}
-      size="sm"
-    >
-      {link.label}
-    </Anchor>
-  ))
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ))
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    )
+  })
 
   return (
     <footer className={classes.footer}>
-      <Grid>
-        <Grid.Col span={3}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
           <Image src={FloraGenicLogo} alt="FloraGenic Logo" width={120} />
-          <Text
-            style={{
-              fontSize: '14px',
-              fontWeight: 500,
-              color: 'rgba(0, 0, 0, 0.4)',
-              maxWidth: '300px',
-            }}
-          >
+
+          <Text size="xs" color="dimmed" className={classes.description}>
             Shop now to revamp your outdoor space into a stunning oasis with our
             all-in-one gardening solution
           </Text>
-          <Group mt={'md'}>
-            <Button
-              href="#"
-              leftIcon={<FaGooglePlay size={18} />}
-              variant="filled"
-              style={{
-                backgroundColor: '#777',
-                color: '#fff',
-                padding: '10px 20px',
-                borderRadius: '5px',
-                fontSize: '14px',
-                fontWeight: 500,
-                marginRight: '10px',
-              }}
-            >
-              Google Play
-            </Button>
-            <Button
-              href="#"
-              leftIcon={<FaGooglePlay size={18} />}
-              variant="filled"
-              style={{
-                backgroundColor: '#777',
-                color: '#fff',
-                padding: '10px 20px',
-                borderRadius: '5px',
-                fontSize: '14px',
-                fontWeight: 500,
-                marginRight: '10px',
-              }}
-            >
-              App Store
-            </Button>
-          </Group>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Stack>
-            <Text
-              style={{
-                fontSize: '20px',
-                fontWeight: 600,
-                marginBottom: '10px',
-                color: '#000',
-              }}
-            >
-              About Us
-            </Text>
-            {aboutUsItems}
-          </Stack>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Stack>
-            <Text
-              style={{
-                fontSize: '20px',
-                fontWeight: 600,
-                marginBottom: '10px',
-                color: '#000',
-              }}
-            >
-              Customer Care
-            </Text>
-            {customerCareItems}
-          </Stack>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Stack>
-            <Text
-              style={{
-                fontSize: '20px',
-                fontWeight: 600,
-                marginBottom: '10px',
-                color: '#000',
-              }}
-            >
-              Contact Us
-            </Text>
-            <Text
-              style={{
-                fontSize: '14px',
-                fontWeight: 400,
-                color: 'rgba(0, 0, 0, 0.4)',
-              }}
-            >
-              Zaki Center Room 1, 2nd Floor, 1st Street, i8 Markaz, Islamabad,
-              Pakistan
-            </Text>
-            <Text
-              style={{
-                fontSize: '14px',
-                fontWeight: 500,
-                color: 'rgba(0, 0, 0, 0.4)',
-              }}
-            >
-              Email: floraGenic@gmail.com
-            </Text>
-            <Text
-              style={{
-                fontSize: '14px',
-                fontWeight: 500,
-                color: 'rgba(0, 0, 0, 0.4)',
-              }}
-            >
-              Phone: +92-313-6942069
-            </Text>
-            <Group spacing="xs" position="left" noWrap>
-              <ActionIcon size="lg" variant="default" radius="xl">
-                <AiOutlineTwitter size={18} stroke={1.5} opacity={0.5} />
-              </ActionIcon>
-              <ActionIcon size="lg" variant="default" radius="xl">
-                <BsFacebook size={18} stroke={1.5} opacity={0.5} />
-              </ActionIcon>
-              <ActionIcon size="lg" variant="default" radius="xl">
-                <AiFillInstagram size={18} stroke={1.5} opacity={0.5} />
-              </ActionIcon>
-            </Group>
-          </Stack>
-        </Grid.Col>
-      </Grid>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text color="dimmed" size="sm">
+          © 2023 floragenic.dev. All rights reserved.
+        </Text>
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg">
+            <BsTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <BsFacebook size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <BsInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
     </footer>
   )
 }
