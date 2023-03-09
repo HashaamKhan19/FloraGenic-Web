5a90ec2437b89b6051df9f8c0d3765900a120113
Hashaam Khan <hashaamkhan4247@gmail.com>
2023-03-09 22:32:30 +0500

    added gardener searching and filtering option

--- components/Customer/Gardener/GardenerListings.js
+++ components/Customer/Gardener/GardenerListings.js
@@ -1,158 +1,129 @@
 import {
-  createStyles,
-  Paper,
-  Text,
-  Avatar,
-  Grid,
-  Container,
-  Stack,
-  Rating,
-  Menu,
-  Badge,
-  UnstyledButton,
-  Group,
+  Box,
   Button,
+  Container,
+  Group,
+  Input,
+  Modal,
+  Pagination,
+  SimpleGrid,
+  Stack,
+  Text,
 } from '@mantine/core'
-import { GoChevronDown } from 'react-icons/go'
-import { BiMap } from 'react-icons/bi'
+import Link from 'next/link'
+import { BiFilterAlt, BiSearch } from 'react-icons/bi'
+import GardenerInfoCard from './GardenerInfoCard'
+import { useMediaQuery } from '@mantine/hooks'
+import { useState } from 'react'
+import ByRatings from '../Filters/FilterTypes/ByRatings'
+import ByAvailability from '../Filters/FilterTypes/ByAvailability'
+import ByCity from '../Filters/FilterTypes/ByCity'
 
-const useStyles = createStyles(() => ({
-  card: {
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    // height: 300,
-    maxWidth: 500,
-  },
-}))
-
-const gardeners = [
-  {
-    id: 1,
-    name: 'Hashaam Khan',
-    location: 'Islamabad',
-    rating: 4.5,
-    skills: ['Gardening', 'Landscaping', 'Fertilizer'],
-    price: '5000/Month',
-  },
-  {
-    id: 2,
-    name: 'Abdullah Saad',
-    location: 'Islamabad',
-    rating: 4.8,
-    skills: ['Gardening', 'Weeding', 'Pruning'],
-    price: '7000/Month',
-  },
-  {
-    id: 3,
-    name: 'Tehseen Riaz Abbasi',
-    location: 'Murree',
-    rating: 4.2,
-    skills: ['Gardening', 'Irrigation', 'Pest Control'],
-    price: '6000/Month',
-  },
-]
-
-const GardenerListings = () => {
-  const { classes } = useStyles()
+export default function GardenerListings() {
+  const matches575 = useMediaQuery('(max-width:575px)')
+  const [opened, setOpened] = useState(false)
 
   return (
-    <Container size={'xl'}>
-      <Grid>
-        <Grid.Col>
-          {gardeners.map((gardener) => (
-            <Paper
-              withBorder
-              radius={'md'}
-              sx={{
-                backgroundImage: `url(${'https://images.unsplash.com/photo-1528731708534-816fe59f90cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGlnaHQlMjBncmF5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'})`,
-                position: 'relative',
+    <Container size={'xl'} pt={60} pb={50}>
+      <Stack spacing={'xs'}>
+        <Group position="apart" style={{ width: '100%' }}>
+          <Text
+            style={{
+              fontSize: 26,
+              color: 'darkslategray',
+            }}
+            weight={600}
+          >
+            All Gardeners
+          </Text>
+          <Group
+            noWrap
+            style={{
+              width: matches575 ? '100%' : 'auto',
+            }}
+          >
+            <Input
+              placeholder="search a gardener..."
+              icon={<BiSearch />}
+              styles={(theme) => ({
+                input: {
+                  '&:focus-within': {
+                    borderColor: theme.colors.green[7],
+                  },
+                },
+              })}
+              style={{
+                width: matches575 ? '100%' : 250,
               }}
-              my={'xl'}
-              className={classes.card}
+            />
+            <Button
+              style={{
+                backgroundColor: '#62A82C',
+              }}
+              leftIcon={<BiFilterAlt size={17} />}
+              onClick={() => setOpened(true)}
             >
-              <Stack align="center" p={'sm'} spacing={'xs'}>
-                <Avatar
-                  size={'xl'}
-                  radius={'50%'}
-                  src="https://i.pravatar.cc/300"
-                />
+              Filters
+            </Button>
+          </Group>
+        </Group>
+        <SimpleGrid
+          cols={3}
+          spacing="xl"
+          verticalSpacing={'xs'}
+          breakpoints={[
+            { maxWidth: 'md', cols: 2 },
+            {
+              maxWidth: 'xs',
+              cols: 1,
+            },
+          ]}
+        >
+          {Array(9)
+            .fill(0)
+            .map((_, index) => (
+              <Link
+                href={`/customer/viewGardener/${123}`}
+                key={`gardener-${index}`}
+              >
+                <GardenerInfoCard key={index} />
+              </Link>
+            ))}
+        </SimpleGrid>
+      </Stack>
+      <Group position="right" pt={'xl'}>
+        <Pagination
+          total={10}
+          position="center"
+          styles={(theme) => ({
+            item: {
+              '&[data-active]': {
+                backgroundImage: theme.fn.gradient({
+                  from: '#62A82C',
+                  to: '#62A82C',
+                }),
+              },
+            },
+          })}
+        />
+      </Group>
 
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: '1.5rem',
-                  }}
-                >
-                  {gardener.name}
-                </Text>
-                <Group spacing={2}>
-                  <BiMap />
-                  <Text
-                    style={{
-                      fontWeight: 500,
-                      fontSize: '1.1rem',
-                    }}
-                  >
-                    {gardener.location}
-                  </Text>
-                </Group>
-                <Rating
-                  value={gardener.rating}
-                  size="md"
-                  fractions={2}
-                  readOnly
-                  mb={5}
-                />
-
-                <Menu
-                  width={170}
-                  shadow="md"
-                  transition={'scale-y'}
-                  transitionDuration={300}
-                  exitTransitionDuration={100}
-                  withArrow
-                >
-                  <Menu.Target>
-                    <UnstyledButton>
-                      <Badge
-                        variant="outline"
-                        pr={3}
-                        rightSection={
-                          <GoChevronDown
-                            style={{
-                              paddingRight: 3,
-                            }}
-                          />
-                        }
-                      >
-                        Skills
-                      </Badge>
-                    </UnstyledButton>
-                  </Menu.Target>
-
-                  <Menu.Dropdown>
-                    {gardener.skills.map((skill) => (
-                      <Menu.Item key={skill}>{skill}</Menu.Item>
-                    ))}
-                  </Menu.Dropdown>
-                </Menu>
-                <Text
-                  style={{
-                    fontSize: '1.2rem',
-                    fontWeight: 500,
-                    color: '#D92228',
-                  }}
-                  mt={'xs'}
-                >
-                  {gardener.price}
-                </Text>
-              </Stack>
-            </Paper>
-          ))}
-        </Grid.Col>
-      </Grid>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="FILTERS"
+        transition={'fade'}
+        transitionDuration={700}
+        transitionTimingFunction="ease"
+        exitTransitionDuration={700}
+        centered
+      >
+        <Box pl={8}>
+          <ByCity />
+        </Box>
+        <ByRatings />
+        <ByAvailability />
+      </Modal>
     </Container>
   )
 }
-
-export default GardenerListings
