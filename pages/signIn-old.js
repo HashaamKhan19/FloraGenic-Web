347f594bca244187f8f062f5a4faa2933124ae36
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-03-19 18:09:53 +0500

    Added new login screen

---
+++ pages/signIn-old.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignInCard from '../components/Generic/pfpManagement/SignInCard'
+
+const signIn = () => {
+  return (
+    <div>
+      <SignInCard />
+    </div>
+  )
+}
+export default signIn
