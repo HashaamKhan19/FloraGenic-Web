9ee5efb7aa1ce9b4a8fbf689098c139d20ba9018
hashaam <hashaamkhan4247@gmail.com>
2023-05-11 23:54:56 +0500

    added nursery login page

---
+++ pages/loginNursery.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NurseryLogin from "../components/Nursery/NurseryLogin";
+
+export default function loginNursery() {
+  return (
+    <>
+      <NurseryLogin />
+    </>
+  );
+}
--- pages/nursery/index.js
+++ pages/nursery/index.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Dashboard from "../../components/Nursery/Dashboard";
+import { useRouter } from "next/router";
+import { AuthContext } from "../../context/authContext";
+import { CircularProgress } from "@mui/material";
 
-const index = () => {
-  return (
-    <>
-      <Dashboard />
-    </>
-  );
+const Index = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const checkUserType = () => {
+      const userType = user?.userType || localStorage.getItem("userType");
+      const token = user?.token || localStorage.getItem("token");
+
+      if (!token || !userType) {
+        localStorage.clear();
+        router.push("/loginNursery");
+      } else if (userType === "NurseryOwner") {
+        setLoading(false);
+        router.push("/nursery");
+      } else {
+        setLoading(false);
+        router.push("/");
+      }
+    };
+
+    checkUserType();
+  }, []);
+
+  return <>{loading ? <CircularProgress size={50} /> : <Dashboard />}</>;
 };
 
-export default index;
+export default Index;
