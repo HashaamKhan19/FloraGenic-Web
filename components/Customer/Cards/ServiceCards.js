6247d8f9c9578c62c884dc3c6255d0c2d8b596c1
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-06-06 23:09:27 +0500

    added green border

--- components/Customer/Cards/ServiceCards.js
+++ components/Customer/Cards/ServiceCards.js
@@ -6,55 +6,55 @@ import {
   SimpleGrid,
   Stack,
   Text,
-} from '@mantine/core'
-import { useMediaQuery } from '@mantine/hooks'
-import { AiOutlineDollar } from 'react-icons/ai'
-import { BiTimer } from 'react-icons/bi'
-import { BsTruck } from 'react-icons/bs'
-import { GiReceiveMoney } from 'react-icons/gi'
-import { MdOutlineFactCheck } from 'react-icons/md'
-import { RiSecurePaymentLine } from 'react-icons/ri'
+} from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { AiOutlineDollar } from "react-icons/ai";
+import { BiTimer } from "react-icons/bi";
+import { BsTruck } from "react-icons/bs";
+import { GiReceiveMoney } from "react-icons/gi";
+import { MdOutlineFactCheck } from "react-icons/md";
+import { RiSecurePaymentLine } from "react-icons/ri";
 
 const mockdata = [
   {
-    title: 'Fast Delivery',
-    description: 'Start from Rs. 99',
+    title: "Fast Delivery",
+    description: "Start from Rs. 99",
     icon: BsTruck,
   },
   {
-    title: 'Money Guarantee',
-    description: '7 Days Back',
+    title: "Money Guarantee",
+    description: "7 Days Back",
     icon: GiReceiveMoney,
   },
   {
-    title: '3 Days',
-    description: 'For Free Return',
+    title: "3 Days",
+    description: "For Free Return",
     icon: BiTimer,
   },
   {
-    title: 'Payment',
-    description: 'Secure System',
+    title: "Payment",
+    description: "Secure System",
     icon: RiSecurePaymentLine,
   },
-]
+];
 
 const useStyles = createStyles((theme) => ({
   card: {
     border: `1px solid ${theme.colors.gray[3]}`,
-    transition: 'all 0.3s ease',
-    '&:hover': {
-      transform: 'translateY(-10px)',
+    transition: "all 0.3s ease",
+    "&:hover": {
+      transform: "translateY(-10px)",
     },
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
-}))
+}));
 
 export default function ServiceCards() {
-  const match768 = useMediaQuery('(max-width: 768px)')
+  const match768 = useMediaQuery("(max-width: 768px)");
 
-  const { classes } = useStyles()
+  const { classes } = useStyles();
   const features = mockdata.map((feature) => (
     <Card
       key={feature.title}
@@ -63,18 +63,20 @@ export default function ServiceCards() {
       className={classes.card}
       style={{
         height: 100,
-        width: match768 ? '100%' : 295,
+        width: match768 ? "100%" : 295,
+        border: "1px solid #62a82c",
+        boxShadow: "0px 5px 5px 0px #62a82c99",
       }}
     >
       <Group>
-        <Group pl={'xs'}>
-          <feature.icon size={50} stroke={2} color={'gray'} />
+        <Group pl={"xs"}>
+          <feature.icon size={50} stroke={2} color={"gray"} />
         </Group>
         <Stack spacing={2}>
           <Text
             size="lg"
             style={{
-              color: 'darkslategray',
+              color: "darkslategray",
               fontWeight: 500,
             }}
           >
@@ -83,7 +85,7 @@ export default function ServiceCards() {
           <Text
             size="sm"
             style={{
-              color: 'darkslategray',
+              color: "darkslategray",
               fontWeight: 400,
             }}
           >
@@ -92,9 +94,9 @@ export default function ServiceCards() {
         </Stack>
       </Group>
     </Card>
-  ))
+  ));
   return (
-    <Container size="xl" mt={80} pb={'xl'}>
+    <Container size="xl" mt={80} pb={"xl"}>
       <SimpleGrid
         cols={4}
         spacing="xl"
@@ -103,10 +105,10 @@ export default function ServiceCards() {
           { maxWidth: 940, cols: 2 },
           { maxWidth: 1200, cols: 3 },
         ]}
-        px={'lg'}
+        px={"lg"}
       >
         {features}
       </SimpleGrid>
     </Container>
-  )
+  );
 }
